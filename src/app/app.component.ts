ac8b160de9f6638d138d33340caf7464618d4149
Anil Kumar Pandiya <anilpandiya05@gmail.com>
2018-12-06 20:28:44 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { ViewChild } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: []
 })
 
 export class AppComponent {
